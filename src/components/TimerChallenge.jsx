5edc04c28519e1f0c75e8612cc345e0b0fabc76f
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-04-23 11:17:33 -0300

    apply useImperativeHandle to detatch the dialog from the Timer component, exposing callable functions

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -7,12 +7,12 @@ export default function TimerChallenge({ title, targetTime }) {
   const [timerExpired, setTimerExpired] = useState(false)
 
   const timer = useRef()
-  const dialogue = useRef()
+  const dialog = useRef()
 
   function handleStart() {
     timer.current = setTimeout(() => {
       setTimerExpired(true)
-      dialogue.current.showModal()
+      dialog.current.open()
     }, targetTime * 1000)
 
     setTimerStarted(true)
@@ -25,7 +25,7 @@ export default function TimerChallenge({ title, targetTime }) {
 
   return (
     <>
-      <ResultModal ref={dialogue} targetTime={targetTime} result='lost' />
+      <ResultModal ref={dialog} targetTime={targetTime} result='lost' />
       <section className='challenge'>
         <h2>{title}</h2>
         <p className='challenge-time'>
